65bc8f4b15a7501543ce39f7291055231df95ee6
ZuleidyYaruro <zvyaruro@gmail.com>
2022-01-05 13:41:03 -0500

    creating routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
+
 // requires
 require('dotenv').config();
 const express=require('express');
+const routes=require('./routes/app.routes');
 
 // app init
 const app=express();
+
+// middlewares
+
+// routes
+app.use('/', routes);
 
 module.exports=app;
